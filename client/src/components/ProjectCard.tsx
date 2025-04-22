fea508ad9082f0c9b555f83716181be066ee5e40
Daisy Choi <chj15937@gmail.com>
2025-04-22 22:04:58 +1200

    update aboutme

--- client/src/components/ProjectCard.tsx
+++ client/src/components/ProjectCard.tsx
@@ -1,13 +1,19 @@
-import { Project } from "../models/Projects"
-
+import { Project } from '../models/Projects'
 
 export default function ProjectCard({ project }: { project: Project }) {
   return (
     <div className="bg-white p-4 rounded shadow mb-4">
-      <h2 className="text-lg font-bold">{project.name}</h2>
+      <h2 className="text-[30px] font-bold">{project.name}</h2>
       <p>{project.description}</p>
-      <p>Tech Stack: {project.techStack.map(tech => <p>{tech}</p>)}</p>
-      <p>{project.url}</p>
+      <p className='text-[20px] font-bold'>Tech Stack:</p>
+      <ul className="grid grid-cols-2 gap-4">
+        {project.techStack.map((tech, index) => (
+          <li key={index} className='border-2 border-[rgb(229,231,235)]'>{tech}</li>
+        ))}
+      </ul>
+      <p className='font-bold text-[20px]'>
+        <a href={project.url}>Visit Me!</a>
+      </p>
     </div>
   )
 }
