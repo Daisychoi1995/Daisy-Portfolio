5958a0c4bacb88e6a4435b9faecdbe139e107b1d
Daisy Choi <chj15937@gmail.com>
2025-04-23 17:20:00 +1200

    add styling

--- client/src/components/MainProjectCard.tsx
+++ client/src/components/MainProjectCard.tsx
@@ -4,6 +4,7 @@ export default function MainProjectCart() {
   return (
     <div className="w-full h-full bg-white p-4 rounded shadow mb-4">
       <p>Welcome to my portfolio! </p>
+      <p></p>
     </div>
   )
 }
--- client/src/pages/AboutMe.tsx
+++ client/src/pages/AboutMe.tsx
@@ -11,14 +11,14 @@ const AboutMe = ({ onClose }: AboutMeProps) => {
 
   return (
     <div
-          className={clsx(
-            'bg-[rgb(229,231,235)] fixed left-1/2 transform -translate-x-1/2 rounded-lg',
-            isMaximize
-              ? 'w-full h-[85%] mb-20'
-              : 'w-[100%] max-w-[900px] h-[70%] top-20',
-            isMinimize && 'h-[40px]'
-          )}
-        >
+      className={clsx(
+        'bg-[rgb(229,231,235)] fixed left-1/2 transform -translate-x-1/2 rounded-lg',
+        isMaximize
+          ? 'w-full h-[85%] mb-20'
+          : 'w-[100%] max-w-[900px] h-[70%] top-20',
+        isMinimize && 'h-[40px]'
+      )}
+    >
       <div className="w-full h-full flex flex-col ">
         <TitleBar
           onClose={onClose}
@@ -31,7 +31,7 @@ const AboutMe = ({ onClose }: AboutMeProps) => {
             setIsMaximize(true)
           }}
         />
-        <div className="flex flex-col p-4">
+        <div className="flex flex-col p-12">
           <div className="flex gap-4 mb-6">
             <img
               src="/assets/main.jpg"
@@ -39,10 +39,19 @@ const AboutMe = ({ onClose }: AboutMeProps) => {
             />
             <div className="flex flex-col">
               <h1 className="text-[70px] font-bo">Daisy Choi</h1>
-              <h2 className='text-[30px]'>Software Developer</h2>
+              <h2 className="text-[30px]">Software Developer</h2>
             </div>
           </div>
-          <p className="text-[20px] ">I believe the best developers are those who never stop learning and I bring that mindset into every line of code. My journey into software engineering began with threebarfifty, a small dessert shop I started. Frustrated by the limitations of platforms like Wix, I wanted more control, which led me to discover my passion for coding. Since then, I've been continuously learning and growing, combining my technical skills with my entrepreneurial spirit to create impactful solutions.</p>
+          <p className="text-[20px] ">
+            I believe the best developers are those who never stop learning and
+            I bring that mindset into every line of code. My journey into
+            software engineering began with threebarfifty, a small dessert shop
+            I started. Frustrated by the limitations of platforms like Wix, I
+            wanted more control, which led me to discover my passion for coding.
+            Since then, I've been continuously learning and growing, combining
+            my technical skills with my entrepreneurial spirit to create
+            impactful solutions.
+          </p>
         </div>
       </div>
     </div>
