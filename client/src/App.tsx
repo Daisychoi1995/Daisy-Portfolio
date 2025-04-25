f449bbc8d44fbbc273bab23dfd07a63ad89667b7
Daisy Choi <chj15937@gmail.com>
2025-04-25 12:38:23 +1200

    delete gallery

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,12 @@ import { useState } from 'react'
 import AboutMe from './pages/AboutMe'
 import MenuBar from './components/MenuBar'
 import Projects from './pages/Projects'
-import Gallery from './pages/Gallery'
+// import Gallery from './pages/Gallery'
 import MessageMe from './pages/MessageMe'
 
 //bg-[url('/assets/wallpaper.jpg)']
 function App() {
-  const [activeMenu, setActiveMenu] = useState('')
+  const [activeMenu, setActiveMenu] = useState('about')
   return (
       <div className="bg-[url('/assets/wallpaper.jpg')] bg-cover bg-center h-screen">
         
@@ -15,7 +15,7 @@ function App() {
         {activeMenu === 'about' && <AboutMe onClose={() => setActiveMenu('')}/>}
         {activeMenu === 'project' && <Projects onClose={() => setActiveMenu('')} />}
         {activeMenu === 'message' && <MessageMe onClose={() => setActiveMenu('')} />}
-        {activeMenu === 'gallery' && <Gallery onClose={() => setActiveMenu('')} />}
+        {/* {activeMenu === 'gallery' && <Gallery onClose={() => setActiveMenu('')} />} */}
  
       </div>
 
--- client/src/components/MenuBar.tsx
+++ client/src/components/MenuBar.tsx
@@ -10,7 +10,7 @@ export default function MenuBar({ onSelect }: Props) {
         <img src='/assets/icons/icon-contacts.png' alt='contacts' onClick={() => onSelect('about')} className="w-[80px] h-auto hover:scale-110 transition"/>
         <img src='/assets/icons/icon-finder.png' alt='finder' onClick={() => onSelect('project')} className="w-[80px] h-auto hover:scale-110 transition"/>
         <img src='/assets/icons/icon-message.png' alt='message' onClick={() => onSelect('message')} className="w-[80px] h-auto hover:scale-110 transition"/>
-        <img src='/assets/icons/icon-photos.png' alt='photos' onClick={() => onSelect('gallery')} className="w-[80px] h-auto hover:scale-110 transition"/>
+        {/* <img src='/assets/icons/icon-photos.png' alt='photos' onClick={() => onSelect('gallery')} className="w-[80px] h-auto hover:scale-110 transition"/> */}
         <a href='https://github.com/Daisychoi1995' target="_blank"><img src='/assets/icons/icon-git.png' alt='github' className="w-[80px] h-auto hover:scale-110 transition"/></a>
         <a href='https://www.linkedin.com/in/daisy-choi-248306339' target="_blank"><img src='/assets/icons/icon-linked-in.png' alt='linked-in' className="w-[80px] h-auto hover:scale-110 transition"/></a>
       </nav>
